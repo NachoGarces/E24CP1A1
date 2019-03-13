692381e3b0922525ae99316e1395872f264cc08e
ignacio <nachogarces15@gmail.com>
2019-03-13 01:49:15 -0300

    Colores dinamicos en numeros - barra de color centralizada - color en las canvas

--- app/assets/javascripts/rgb.js
+++ app/assets/javascripts/rgb.js
@@ -4,6 +4,12 @@ $(function(){
     'max':255,
     'change': function(){
       var cssRgb = 'rgb(' + $('#red').val() + ',' + $('#green').val() + ',' + $('#blue').val() + ')';
+      var cssRed = 'rgb(' + $('#red').val() + ', 0, 0)';
+      var cssGreen = 'rgb(0, ' + $('#green').val() + ', 0)';
+      var cssBlue = 'rgb(0, 0, ' + $('#red').val() + ')';
+      $('#red').css('color', cssRed);
+      $('#green').css('color', cssGreen);
+      $('#blue').css('color', cssBlue);
       $('#cuadradito').css('background-color', cssRgb);
       $('#textito').html(cssRgb);
     }
